c357939ea05e9dce4505f69b5bf4fd11782c4e91
viccabezos <cabezosvic@gmail.com>
2021-05-12 10:05:31 +0200

    pile ou face

---
+++ pileOuface.js
@@ -0,0 +1,16 @@
+let pile = 0;
+let face = 0;
+
+function tirer() {
+  if (Math.random() > 0.5) {
+    pile++;
+    document.getElementById("text").innerHTML = "Pile";
+  } else {
+    face++;
+    document.getElementById("text").innerHTML = "Face";
+  }
+
+  if (pile >= 10) {
+    document.location.href = "index.html";
+  }
+}
